d52a24da9ca1d5f27d979f0f787c87f954c764dc
Mbossowski <92019363+Mbossowski@users.noreply.github.com>
2022-11-17 16:07:33 +0100

    Update main.cpp

--- Zad2/main.cpp
+++ Zad2/main.cpp
@@ -9,6 +9,10 @@ int main()
     Zmienna<double> VarD;
     Zmienna<std::string> VarS;
   
+    VarI.Input(3);
+    VarD.Input(4.5);
+    VarS.Input("Lorem ipsum");
+  
     VarI.Display();
     VarD.Display();
     VarS.Display();
