9057d9e5339c169cd1602ff1c216ed953eb7734d
Mbossowski <92019363+Mbossowski@users.noreply.github.com>
2022-12-13 19:13:33 +0100

    Create main.cpp

---
+++ Lab3/main.cpp
@@ -0,0 +1,22 @@
+
+#include "AlgorytmyIteratory.h"
+#include <iostream>
+
+int main()
+{
+	mb::AlgorytmyIteratory Obj;
+
+	Obj.zad2();
+
+	std::cout << std::endl;
+
+	Obj.zad3();
+
+	std::cout << std::endl;
+
+	Obj.zad4();
+	
+	std::cout << std::endl;
+
+	Obj.zad5();
+}
